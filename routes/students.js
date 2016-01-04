ab702139a615950f7efe9c561c2f2ec890c8e0cc
YC123h <512185256@qq.com>
2016-01-04 21:22:00 +0800

    学生ajax登录添加zepto引入

--- routes/students.js
+++ routes/students.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var students = []
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('students/login');
+router.get('/', function (req, res, next) {
+    res.render('students/login');
 });
-router.get('/students/index', function(req, res, next) {
+
+router.post('/', function (req, res,callback) {
+    console.log(req.body)
+    res.send({
+        success:true,
+        text:'登录成功'
+    })
+})
+
+router.get('/students/index', function (req, res, next) {
     res.render('students/index');
 });
-router.get('/students/classInfo', function(req, res, next) {
+router.get('/students/classInfo', function (req, res, next) {
     res.render('students/classInfo');
 });
-router.get('/students/questionInfo', function(req, res, next) {
+router.get('/students/questionInfo', function (req, res, next) {
     res.render('students/questionInfo');
 });
-router.get('/students/classWall', function(req, res, next) {
+router.get('/students/classWall', function (req, res, next) {
     res.render('students/classWall');
 });
 
